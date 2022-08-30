94cac8bb0aa7d660ee9b86c5d40cac07549f8b4f
Enrique Moreno <morenocarmonaenrique@gmail.com>
2022-08-30 10:58:43 +0200

    Chromatic agregado

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,46 @@
+import "./mylabel.css";
+
+export interface MyLabelProps {
+  /**
+   * Este es el mensaje a mostrar en la etiqueta
+   */
+  label: string;
+  /**
+   * Este es el tamaño de la etiqueta
+   */
+  size: "normal" | "h1" | "h2" | "h3";
+  /**
+   * Indica si está capitalizado
+   */
+  allCaps?: boolean;
+  /**
+   * Indica el color del componente
+   */
+  color?: "primary" | "secondary" | "tertiary";
+  /**
+   * Color personalizado de la fuente
+   */
+  fontColor?: string;
+  /**
+   * Color personalizado del fondo
+   */
+  backgroundColor?: string;
+}
+
+export const MyLabel = ({
+  label = "No Label",
+  size = "normal",
+  allCaps = false,
+  color = "primary",
+  fontColor,
+  backgroundColor = "transparent",
+}: MyLabelProps) => {
+  return (
+    <span
+      className={`label ${size} text-${color}`}
+      style={{ color: fontColor, backgroundColor }}
+    >
+      {allCaps ? label.toUpperCase() : label}
+    </span>
+  );
+};
